94784d0e3a787f299a6d4348b0cce4bdbb51b0e9
gibithai <dougcarlin@tut.by>
2024-02-04 17:13:40 +0300

    Add .gitlab-ci.yml and configure pipeline

--- cypress/e2e/2FA/2FA.cy.js
+++ cypress/e2e/2FA/2FA.cy.js
@@ -2,7 +2,7 @@ describe ('Conditions testing', () => {
   const username = 'dougcarlin@tut.by'
   const userpassword = 'j7878745g655756755'
 
-  it ('2FA window check', ()=>{
+  it.skip ('2FA window check', ()=>{
     cy.visit('https://walletallin.com/')
     cy.xpath('//a[normalize-space()="Login"]').click()
     cy.visit('https://wallet.walletallin.com/')
@@ -12,7 +12,7 @@ describe ('Conditions testing', () => {
     
     cy.xpath('//div[@role="dialog"]').should('be.visible').then(() => {
   // Этот блок кода выполнится, если элемент виден (то есть окно появилось)
-  cy.wait(2000)
+  cy.wait(1000)
   // Проверка существования элемента
   cy.xpath('//div[@role="dialog"]').then((twoFAelement) => {
     // Если элемент существует (первое условие)
@@ -23,8 +23,22 @@ describe ('Conditions testing', () => {
     } else if (twoFAelement.length < 0) {
       cy.reload()
       cy.wait(1000)
-    } 
+    }
   })
 })
+})
+  it.only ('2FA window check', ()=>{
+    cy.visit('https://walletallin.com/')
+    cy.xpath('//a[normalize-space()="Login"]').click()
+    cy.visit('https://wallet.walletallin.com/')
+    cy.get('#email').type(username)
+    cy.get('#password').type(userpassword)
+    cy.xpath('//button[@id=":r2:"]').click()
+    cy.xpath('//div[@role="dialog"]')
+    if (Math.round(Math.random()) === 1) {
+      cy.xpath('//button[normalize-space()="Yes"]').click()
+    } else {
+      cy.xpath('//button[@class="swal2-cancel swal2-styled"]').click()
+    }
 })
 })
